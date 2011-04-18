c262eeb4ce7e85f122702fd886de0f8e0716117d
Henrik Vendelbo <henrik@thepia.com>
2011-04-17 20:36:57 -0400

    50 - 50

---
+++ js/fixes.js
@@ -0,0 +1,3 @@
+
+// background image cache bug for ie6.  via: http://www.mister-pixel.com/#Content__state=
+/*@cc_on   @if (@_win32) { document.execCommand("BackgroundImageCache",false,true) }   @end @*/
