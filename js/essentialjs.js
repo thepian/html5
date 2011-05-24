f365e97c610580fbef5356187fc2653be91edb3e
Henrik Vendelbo <henrik@thepia.com>
2011-05-24 16:54:36 -0300

    minor fixes

--- js/essentialjs.js
+++ js/essentialjs.js
@@ -2,7 +2,7 @@
     
 if (/ no-horizontal-scroll/.test(document.documentElement.className)) {
     $(window).bind("scroll",function(event){
-        console.log(document.documentElement.scrollTop+" "+document.documentElement.scrollLeft);
+        // console.log(document.documentElement.scrollTop+" "+document.documentElement.scrollLeft);
         document.documentElement.scrollLeft = 0; // no horizontal scroll
     });
 }
