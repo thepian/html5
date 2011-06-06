0f7285bede1a9a7d67baefb7a39cf41557536f09
Henrik Vendelbo <henrik@thepia.com>
2011-06-05 23:21:25 -0300

    Add gradient in IE, no image back to top expanding search input text-shadow: white and black shadow

---
+++ _design/inspiration/CSS3 Gradient Buttons_files/hostedbadge.php
@@ -0,0 +1,6 @@
+
+	function writeSuBadge () { 
+		var bdg = "<iframe src=\"http:\/\/www.stumbleupon.com\/badge\/embed\/2\/?url=http%3A%2F%2Fwebdesignerwall.com%2Ftutorials%2Fcss3-gradient-buttons\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; width:65px; height: 18px;\" allowTransparency=\"true\"><\/iframe>";
+		document.write(bdg);
+	}
+	writeSuBadge();
---
+++ _design/inspiration/CSS3 Gradient Buttons_files/v6.php
@@ -0,0 +1,3 @@
+/*1307215925,171374134*/
+
+window._ip_version = {"address":"189.104.225.113","version":4,"variant":null};window._ip_w["check6"] = 0;
